eba1ed01496978e312803c43bec3c830f81d1cb9
Samplats79 <samplatsasubonteng124@gmail.com>
2025-07-07 15:04:15 +0200

    fix: database connectie geüpdatet voor Railway

--- database.php
+++ database.php
@@ -1,16 +1,18 @@
 <?php
 class Database {
-    private $host = 'localhost'; 
-    private $dbname = 'todo'; // dit is jouw databanknaam
+    private $host = 'maglev.proxy.rlwy.net';
+    private $port = 48028;
+    private $dbname = 'railway';
     private $username = 'root';
-    private $password = ''; 
+    private $password = 'zBtImbWqqbjTqpcreTzBDcBvzAhLEJxD';
     private $conn;
 
     public function connect() {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->username, $this->password);
+            $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->dbname;charset=utf8";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Verbinding mislukt: " . $e->getMessage();
