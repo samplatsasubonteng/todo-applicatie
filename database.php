51811adac54e913518b2a1a4addcc5496fa0543f
Samplats79 <samplatsasubonteng124@gmail.com>
2025-08-27 16:25:01 +0200

    fix: database terug geconnecteerd met railway

--- database.php
+++ database.php
@@ -1,16 +1,18 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $dbname = 'todo'; // pas aan naar jouw lokale database
+    // Gegevens rechtstreeks uit je Railway MYSQL vars
+    private $host = 'shortline.proxy.rlwy.net';
+    private $port = 40104; // dit is het stukje na de :
+    private $dbname = 'railway'; 
     private $username = 'root';
-    private $password = '';
+    private $password = 'SRrXpAvTyXFdpQniREtlIbqwBLDthm'; // exact uit MYSQL_ROOT_PASSWORD
     private $conn;
 
     public function connect() {
         $this->conn = null;
 
         try {
-            $dsn = "mysql:host=$this->host;dbname=$this->dbname;charset=utf8";
+            $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->dbname};charset=utf8mb4";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
